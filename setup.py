db1dcd9e1aa4fb28fa07f50c37082a3853deb35c
Nicolas Law-dune <nicolas@synapsium.com>
2024-11-30 09:58:13 +0100

    update version

--- setup.py
+++ setup.py
@@ -17,9 +17,13 @@ from setuptools import setup, find_packages  # noqa: H301
 import sys
 
 ####### get version from arguments start ##########
-index = sys.argv.index('--version')
-sys.argv.pop(index)
-version = sys.argv.pop(index)
+version = None
+if '--version' in sys.argv:
+    index = sys.argv.index('--version')
+    sys.argv.pop(index)
+    version = sys.argv.pop(index)
+else:
+    version = "1.0.0"
 ####### get version from arguments end ##########
 
 # To install the library, run the following
